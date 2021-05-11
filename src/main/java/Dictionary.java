f5ebf669a12eadbff8abdfdc7638dc39e77c16d6
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-05-11 10:01:06 +0200

    Refactoring of method to add document to dictionary

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -14,8 +14,8 @@ public class Dictionary {
 
     }
 
-    public static void addDocumentAtDictionary(String nameDoc) {
-        String docID = UUID.nameUUIDFromBytes(nameDoc.getBytes()).toString().substring(0, 8);
+    public static void addDocumentAtDictionary(String docPath) {
+        String docID = UUID.nameUUIDFromBytes(docPath.getBytes()).toString().substring(0, 8);
 
         // TODO: is UUID really unique if we cast in to 8 characters? -> this doubt bring to this 'for' of control
 //        for (int i = 0; i < documentList.size(); i++) {
@@ -25,19 +25,13 @@ public class Dictionary {
 //            }
 //        }
 
+        //TODO: control if the doc is already present in the dictionary
         documentList.add(docID);
 
-        Scanner inputStream = null;
-        try {
-            inputStream = new Scanner(new File(nameDoc));
-        } catch (FileNotFoundException e) {
-            System.out.println("Document not found " + nameDoc);
-            System.exit(0);
-        }
-        while (inputStream.hasNext()) {
-            String token = inputStream.next();
-            ArrayList<String> postingList = new ArrayList<>();
+        ArrayList<String> tokenList = getListOfTokenFromFile(docPath);
+        ArrayList<String> postingList = new ArrayList<>();
 
+        for (String token : tokenList) {
             if (dictionary.containsKey(token)) {
                 postingList = dictionary.get(token);
                 if (!postingList.contains(docID)) {
@@ -48,8 +42,24 @@ public class Dictionary {
                 postingList.add(docID);
                 dictionary.put(token, postingList);
             }
+        }
+    }
 
+    private static ArrayList<String> getListOfTokenFromFile(String docPath) {
+        Scanner inputStream = null;
+        try {
+            inputStream = new Scanner(new File(docPath));
+        } catch (FileNotFoundException e) {
+            System.out.println("Document not found " + docPath);
+            System.exit(0);
+        }
+
+        ArrayList<String> tokenList = new ArrayList<>();
+        while (inputStream.hasNext()) {
+            String token = inputStream.next();
+            tokenList.add(token);
         }
         inputStream.close();
+        return tokenList;
     }
 }
