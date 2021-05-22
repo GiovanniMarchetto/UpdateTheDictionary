30cd8c7f7bc098e0f55c41e9afb6361dc188184e
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2021-05-22 15:54:22 +0200

    Better test for remove words

--- src/test/java/miscTest.java
+++ src/test/java/miscTest.java
@@ -1,6 +1,8 @@
-import dataStructure.Dictionary;
 import miscellaneous.Miscellaneous;
+import operations.PhrasalQueries;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,17 +14,22 @@ public class miscTest {
 
         String token = "good";
         String normToken = Miscellaneous.getNormalizeToken(token);
-        assertEquals("good",normToken);
+        assertEquals("good", normToken);
     }
 
     @Test
     void removeStopWords() {
         Miscellaneous.testTitleFormatting("REMOVE STOP WORDS");
+        ArrayList<String> phrase = PhrasalQueries.getTokenListFromPhrase("The pen is on the table");
+        System.out.println(phrase);
+        assertEquals(6, phrase.size());
+        Miscellaneous.removeStopWords(phrase);
+        System.out.println(phrase);
 
-        Dictionary dictionary = new Dictionary();
-        String docPath = System.getProperty("user.dir") + "\\src\\test\\resources\\" + "A.txt";
-        dictionary.addDocumentAtDictionary(docPath);
-        assertEquals(1, dictionary.getDocumentList().size());
+//        Dictionary dictionary = new Dictionary();
+//        String docPath = System.getProperty("user.dir") + "\\src\\test\\resources\\" + "A.txt";
+//        dictionary.addDocumentAtDictionary(docPath);
+//        assertEquals(1, dictionary.getDocumentList().size());
 //        dictionary.printDictionary();
     }
 
