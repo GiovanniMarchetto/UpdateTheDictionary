c02859509c68f6e40e24838c430cbc7749e7df0f
GiovanniMarchetto <73216336+GiovanniMarchetto@users.noreply.github.com>
2022-03-23 12:15:06 +0100

    Better normalization

--- src/main/java/operations/Normalization.java
+++ src/main/java/operations/Normalization.java
@@ -3,29 +3,43 @@ package operations;
 import java.util.ArrayList;
 
 public class Normalization {
-    private static final String[] SPECIAL_CHARACTERS = {
-            ".", ",", ";", ":", "(", ")", "!", "?", "/", "[", "]", "{", "}",
-            "'", "\""
-    };
+
 
     public static void normalizeListOfString(ArrayList<String> stringArrayList) {
+        String halfWord = "";
+        boolean halfWordFind = false;
+
         for (int i = 0, stringArrayListSize = stringArrayList.size(); i < stringArrayListSize; i++) {
-            stringArrayList.set(i, getNormalizeToken(stringArrayList.get(i)));
+            String normalizeToken = getNormalizeToken(stringArrayList.get(i));
+
+            if (!normalizeToken.equals("")) {
+
+                if (normalizeToken.endsWith("-") && i != stringArrayList.size() - 1) {
+                    halfWord = normalizeToken.substring(0, normalizeToken.length() - 2);
+                    halfWordFind = true;
+                    continue;
+                }
+
+                if (halfWordFind) {
+                    normalizeToken = halfWord + normalizeToken;
+                    halfWord = "";
+                    halfWordFind = false;
+                }
+
+                stringArrayList.set(i, normalizeToken);
+            }
         }
     }
 
     public static String getNormalizeToken(String token) {
         token = token.toLowerCase();// no distinction of names
-        //TODO: plural?
 
-        //TODO: special character also in the middle of the words? and 's.. ecc
-        for (String suffix : SPECIAL_CHARACTERS) {
-            if (token.startsWith(suffix)) {
-                token = token.substring(1);
-            }
-            if (token.endsWith(suffix)) {
-                token = token.substring(0, token.length() - 1);
-            }
+        //it's a dictionary, so no numbers
+        token = token.replaceAll("[^a-z'-]", "");
+        //TODO: plural-porter stemmer
+
+        if (token.equals("'") || token.equals("-")) {
+            token = "";
         }
 
         return token;
